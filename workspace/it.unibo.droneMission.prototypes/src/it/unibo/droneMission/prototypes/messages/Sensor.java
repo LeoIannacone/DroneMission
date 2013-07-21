8c530fbbca4ca12f28cf045e4628b9ff34278f1c
Leo Iannacone <leo.iannacone@gmail.com>
2013-07-21 15:40:16 +0200

    added to string method

--- workspace/it.unibo.droneMission.prototypes/src/it/unibo/droneMission/prototypes/messages/Sensor.java
+++ workspace/it.unibo.droneMission.prototypes/src/it/unibo/droneMission/prototypes/messages/Sensor.java
@@ -7,4 +7,8 @@ public class Sensor extends MessageTypeAsIntValueAsString implements ISensor {
 	public Sensor(int type, String value) {
 		super(type, value);
 	}
+	
+	public String toString() {
+		return String.format("SENSOR: type[%d] - value[%s]", type, value);
+	}
 }
