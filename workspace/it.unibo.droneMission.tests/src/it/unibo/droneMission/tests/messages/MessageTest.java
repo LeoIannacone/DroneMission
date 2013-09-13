85679269536693d88de741fd5e12515a28b97ab2
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-13 14:59:14 +0200

    testing some file convertion

--- workspace/it.unibo.droneMission.tests/src/it/unibo/droneMission/tests/messages/MessageTest.java
+++ workspace/it.unibo.droneMission.tests/src/it/unibo/droneMission/tests/messages/MessageTest.java
@@ -1,9 +1,11 @@
 package it.unibo.droneMission.tests.messages;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -15,11 +17,10 @@ import it.unibo.droneMission.gauge.GaugeValueInt;
 import it.unibo.droneMission.gauge.Odometer;
 import it.unibo.droneMission.gauge.Speedometer;
 import it.unibo.droneMission.interfaces.messages.TypesCommand;
-import it.unibo.droneMission.prototypes.messages.Command;
-import it.unibo.droneMission.prototypes.messages.Factory;
-import it.unibo.droneMission.prototypes.messages.Message;
+import it.unibo.droneMission.prototypes.messages.File;
 import it.unibo.droneMission.prototypes.messages.Sensor;
 import it.unibo.droneMission.prototypes.messages.SensorsData;
+import it.unibo.droneMission.prototypes.messages.Utils;
 public class MessageTest {
 
 	/**
@@ -27,7 +28,36 @@ public class MessageTest {
 	 */
 	public static void main(String[] args) {
 
-		testSensorsData();
+		testFile();
+	}
+	
+	public static void testFile() {
+		
+		String fileToEncode = "/home/l3on/tmp/image.jpg";
+		String fileDecoded = "/home/l3on/tmp/test.jpg";
+		
+		File f = new File(fileToEncode);
+		java.io.File file = new java.io.File(fileToEncode);
+		String base64 = Utils.encodeFileToBase64(file);
+		System.out.println(base64);
+		byte[] content = Utils.decodeFileFromBase64(base64);
+		
+		try {
+			java.io.File test = new java.io.File(fileDecoded);
+			test.createNewFile();
+			FileOutputStream out = new FileOutputStream(test);
+			out.write(content);
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println(f.toJSON());
 	}
 	
 	public static void testSensorsData() {
@@ -62,7 +92,6 @@ public class MessageTest {
 	public static void testSensor( ) {
 		Sensor c = new Sensor(TypesCommand.START_MISSION, "2.34");
 //		c.setValue(3);
-		Gson gson = new Gson();
 		System.out.println(c);
 		String json = c.toJSON();
 		System.out.println(json);
