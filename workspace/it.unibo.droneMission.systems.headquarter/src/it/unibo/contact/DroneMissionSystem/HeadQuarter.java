a54106fca92f0d1a464340d4d97f998ee68d9bc7
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-13 16:30:17 +0200

    start working on headquarter from analysys

---
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/HeadQuarter.java
@@ -0,0 +1,42 @@
+package it.unibo.contact.DroneMissionSystem;
+
+public class HeadQuarter extends HeadQuarterSupport {
+		// Just for testing purpose
+		private int commandCounter;
+		private int MAX_CMD = 10;
+		
+		public HeadQuarter(String s) throws Exception{
+			
+			super(s);
+			commandCounter = 0;
+		}
+
+		@Override
+		protected String getCommandToSend() throws Exception {
+			String cmd;
+			if (commandCounter >= MAX_CMD) {
+				cmd = "stop";
+			}
+			else {
+				commandCounter += 1;
+				cmd = "setspeed " + ((int) (Math.round(Math.random() * 100)) % 60 + 60); 
+			}
+			return cmd;
+		}
+
+		@Override
+		protected void updateDashboard(String dataSensorsReceived)
+				throws Exception {
+			env.println("DATA RECEIVED: " +dataSensorsReceived);
+		}
+
+		@Override
+		protected void storePhotoData(String photoReceived) throws Exception {
+			env.println("PHOTO RECEIVED: " +photoReceived);
+		}
+
+		@Override
+		protected void shutdown() throws Exception {
+			env.println("MISSION END - SHUTDOWN.");			
+		}
+}
---
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/SubSystemDrone.java
@@ -0,0 +1,6 @@
+package it.unibo.contact.DroneMissionSystem;
+//package it.unibo.contact.DroneMissionSystem;
+//
+//public class SubSystemDrone extends SubSystemDroneMain {
+//
+//}
