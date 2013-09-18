4828c42c7c3b891aa7d6c2dd5a31432587ac8f05
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-18 13:35:34 +0200

    workaround: user empty classes in order to export jar

--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/SubSystemHeadQuarter.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/SubSystemHeadQuarter.java
@@ -4,3 +4,7 @@ package it.unibo.contact.DroneMissionSystem;
 //public class SubSystemHeadQuarter extends SubSystemHeadQuarterMain {
 //
 //}
+
+public class SubSystemHeadQuarter {
+	
+}
--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/SubSystemSmartDevice.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/DroneMissionSystem/SubSystemSmartDevice.java
@@ -4,3 +4,7 @@ package it.unibo.contact.DroneMissionSystem;
 //public class SubSystemSmartDevice extends SubSystemSmartDeviceMain {
 //
 //}
+
+public class SubSystemSmartDevice {
+	
+}
