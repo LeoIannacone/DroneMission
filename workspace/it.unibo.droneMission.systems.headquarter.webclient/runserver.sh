786dc96bd4843d8393ad439b7affcd3c7e327ce5
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-18 16:04:55 +0200

    export CLASSPATH to use gson.jar, mysql.jar, and my temporary .jar

--- workspace/it.unibo.droneMission.systems.headquarter.webclient/runserver.sh
+++ workspace/it.unibo.droneMission.systems.headquarter.webclient/runserver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-export CLASSPATH="$CLASSPATH:`pwd`/../../libs/jars/mysql-connector-java-5.1.16.jar"
+
+export CLASSPATH="$CLASSPATH\
+:`pwd`/../../libs/jars/mysql-connector-java-5.1.16.jar\
+:`pwd`/../../libs/jars/gson-2.2.4.jar\
+:`pwd`/../it.unibo.tmp.jar"
+
 export JYTHONPATH="$JYTHONPATH:`pwd`/webclient"
 export DJANGO_SETTINGS_MODULE="webclient.settings"
 source venv/bin/activate
