ce4e9eaad87e5ccfeab59c86a73dfce8532c9cbe
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-18 10:16:34 +0200

    enabled urls

--- workspace/it.unibo.droneMission.systems.headquarter.webclient/webclient/webclient/urls.py
+++ workspace/it.unibo.droneMission.systems.headquarter.webclient/webclient/webclient/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+import settings
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -13,5 +14,10 @@ urlpatterns = patterns('',
     #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    # (r'^admin/', include(admin.site.urls)),
+    
+    # Required to make static serving work 
+    (r'^libs/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    
+    (r'/*', 'headquarter.views.index'),
 )
