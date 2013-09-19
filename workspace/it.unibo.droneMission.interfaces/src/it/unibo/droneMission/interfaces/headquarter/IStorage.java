b592333389bac3a680cf6c90d454a0e5808e3cb5
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-19 10:54:35 +0200

    added new functions to Storage interface - NEEDS MORE WORK

--- workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/IStorage.java
+++ workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/IStorage.java
@@ -1,5 +1,7 @@
 package it.unibo.droneMission.interfaces.headquarter;
 
+import java.util.List;
+
 import it.unibo.droneMission.interfaces.messages.ICommand;
 import it.unibo.droneMission.interfaces.messages.IFile;
 import it.unibo.droneMission.interfaces.messages.INotify;
@@ -16,24 +18,29 @@ public interface IStorage {
 	public void storeCommand(ICommand command);
 	public void storeCommandReply(IReply reply);
 	public ICommand getCommandToSend();
+	public List<ICommand> getLatestCommands(int n);
 	
 	// notify
 	public void storeNotify(INotify notify);
 	public INotify getLatestNotify();
+	public List<INotify> getLatestNotifies(int n);
 	
 	// sensors data
 	public void storeSensorsData(ISensorsData data);
-	public ISensorsData getLatestSensorsData();
 	public ISensorsData getSensorsData(long time);
+	public ISensorsData getLatestSensorsData();
+	public List<ISensorsData> getLatestSensorsDatas(int n);
 	
 	// picture package
 	public void storePicturePackage(IPicturePackage pack);
 	public IPicturePackage getLatestPicturePackage();
+	public List<IPicturePackage> getLatestPicturePackages(int n);
 	
 	// general file
 	public void storeFile(IFile file);
 	public IFile getFile(String filename);
 	public IFile getFile(long time);
+	public List<IFile> getLatestFiles(int n);
 	
 	// for debuggin purpose
 	public void setDebug(int level);
