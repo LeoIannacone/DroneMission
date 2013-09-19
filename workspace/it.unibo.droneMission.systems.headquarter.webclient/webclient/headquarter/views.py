007bb2e9bae74e3cf1295784969e94e38104b2a4
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-19 17:23:53 +0200

    working on getting last sensors

--- workspace/it.unibo.droneMission.systems.headquarter.webclient/webclient/headquarter/views.py
+++ workspace/it.unibo.droneMission.systems.headquarter.webclient/webclient/headquarter/views.py
@@ -6,18 +6,27 @@ from headquarter.models import storage
 from it.unibo.droneMission.prototypes.messages import Utils
 from it.unibo.droneMission.interfaces.messages import TypesSensor
 
-def format_gauges(gauges):
+def format_sensors(sensors):
     formatted_gauges = []
-    for gauge in gauges:
+    lat = 0
+    lon = 0
+    for gauge in sensors.getGauges():
         g = {}
         type = Utils.getGaugeType(gauge)
         g['name'] = Utils.getGaugeName(type)
+        g['value'] = gauge.getCurValRepDisplayed()
         if type == TypesSensor.LOCTRACKER:
-            g['value'] = gauge.toString()
-        else:
-            g['value'] = gauge.getVal().valAsString()
+            lat = gauge.getLat().valAsDouble()
+            lon = gauge.getLon().valAsDouble()
         formatted_gauges.append(g)
-    return formatted_gauges
+    
+    result = {}
+    result["gauges"] = formatted_gauges
+    result["latitude"] = lat
+    result["longitude"] = lon
+    result["time"] = sensors.getTime()
+    
+    return result
 
 def index(request):
        
@@ -26,6 +35,6 @@ def index(request):
 def latest_sensors(request):
     
     sensors = storage.getLatestSensorsData()
-    gauges = format_gauges(sensors.getGauges())
+    f_s = format_sensors(sensors)
     
-    return render_to_response('ajax/sensors_latest.html', {'gauges': gauges})
+    return render_to_response('ajax/sensors_latest.html', f_s)
