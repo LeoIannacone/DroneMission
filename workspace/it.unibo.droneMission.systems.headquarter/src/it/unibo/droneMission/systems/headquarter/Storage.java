7ebf0db3a09d99920876ebe0afd6488d288e0f10
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-19 10:55:15 +0200

    simple functions in general storage

--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/droneMission/systems/headquarter/Storage.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/droneMission/systems/headquarter/Storage.java
@@ -1,6 +1,11 @@
 package it.unibo.droneMission.systems.headquarter;
 
+import java.util.List;
+
 import it.unibo.droneMission.interfaces.headquarter.IStorage;
+import it.unibo.droneMission.interfaces.messages.INotify;
+import it.unibo.droneMission.interfaces.messages.IPicturePackage;
+import it.unibo.droneMission.interfaces.messages.ISensorsData;
 
 public abstract class Storage implements IStorage {
 
@@ -16,6 +21,30 @@ public abstract class Storage implements IStorage {
 		if(level <= DEBUG) {
 			System.err.println(String.format("debug (%d): %s", level, s));
 		}
+	}
+	
+	@Override
+	public INotify getLatestNotify() {
+		List<INotify> list = getLatestNotifies(1);
+		if (list != null && list.size() > 0)
+			return list.get(0);
+		return null;
+	}
+	
+	@Override
+	public ISensorsData getLatestSensorsData() {
+		List<ISensorsData> list = getLatestSensorsDatas(1);
+		if (list != null && list.size() > 0)
+			return list.get(0);
+		return null;
+	}
+
+	@Override
+	public IPicturePackage getLatestPicturePackage() {
+		List<IPicturePackage> list = getLatestPicturePackages(1);
+		if (list != null && list.size() > 0)
+			return list.get(0);
+		return null;
 	}
 
 }
