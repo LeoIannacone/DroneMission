23590b4f07963372d9c5f983502429e6b0707033
Leo Iannacone <leo.iannacone@gmail.com>
2013-09-19 12:57:41 +0200

    removed some unsed import

--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/droneMission/systems/headquarter/DataBase.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/droneMission/systems/headquarter/DataBase.java
@@ -4,9 +4,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Hashtable;
 import java.util.List;
 
