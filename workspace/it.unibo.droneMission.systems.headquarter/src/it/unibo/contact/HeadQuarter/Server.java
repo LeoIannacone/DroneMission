2755c51878f465c215da6799ffbd19e15d4964ff
Leo Iannacone <leo.iannacone@gmail.com>
2013-10-30 15:59:09 +0100

    new files extending auto-src classes

---
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/HeadQuarter/Server.java
@@ -0,0 +1,34 @@
+package it.unibo.contact.HeadQuarter;
+
+public class Server extends ServerSupport {
+
+	public Server(String name) throws Exception {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected int showReplyToCommand(String reply) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected String getSensorsData(String time) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getMission(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getPicturePackage(String time) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
