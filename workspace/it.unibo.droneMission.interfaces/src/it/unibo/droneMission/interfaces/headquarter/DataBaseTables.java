24f27617519473b92a5084b1bfdd02e462852cef
Leo Iannacone <l3on@ubuntu.com>
2013-11-09 15:39:01 +0100

    update database tables reference

--- workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/DataBaseTables.java
+++ workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/DataBaseTables.java
@@ -3,20 +3,23 @@ package it.unibo.droneMission.interfaces.headquarter;
 public class DataBaseTables {
 	
 	// Missions table
-	// FIX_ME: change tables according with new interfaces
+	public static final String MISSIONS_TABLENAME = "missions";
+	public static final String MISSIONS_ID = "id";
+	public static final String MISSIONS_START = "starts";
+	public static final String MISSIONS_END = "end";
 	
 	// Commands table
 	public static final String COMMANDS_TABLENAME = "commands";
 	public static final String COMMANDS_COLUMN_ID = "id";
+	public static final String COMMANDS_COLUMN_MISSION = "mission";
 	public static final String COMMANDS_COLUMN_TYPE = "type";
 	public static final String COMMANDS_COLUMN_TIME = "time";
 	public static final String COMMANDS_COLUMN_VALUE = "value";
-	public static final String COMMANDS_COLUMN_STATUS = "status";
-	
 	
 	// Replies table
 	public static final String REPLIES_TABLENAME = "replies";
 	public static final String REPLIES_COLUMN_ID = "id";
+	public static final String REPLIES_COLUMN_MISSION = "mission";
 	public static final String REPLIES_COLUMN_TYPE = "type";	
 	public static final String REPLIES_COLUMN_VALUE = "value";
 	public static final String REPLIES_COLUMN_COMMAND = "command_id";
@@ -25,6 +28,7 @@ public class DataBaseTables {
 	// Notifies table
 	public static final String NOTIFIES_TABLENAME = "notifies";
 	public static final String NOTIFIES_COLUMN_ID = "id";
+	public static final String NOTIFIES_COLUMN_MISSION = "mission";
 	public static final String NOTIFIES_COLUMN_TYPE = "type";
 	public static final String NOTIFIES_COLUMN_VALUE = "value";
 	public static final String NOTIFIES_COLUMN_TIME = "time";
@@ -32,12 +36,14 @@ public class DataBaseTables {
 	// Sensors table
 	public static final String SENSORS_TABLENAME = "sensors";
 	public static final String SENSORS_COLUMN_ID = "id";
+	public static final String SENSORS_COLUMN_MISSION = "mission";
 	public static final String SENSORS_COLUMN_TIME = "time";
 	public static final String SENSORS_COLUMN_DATA = "data";
 	
 	// Pictures table
 	public static final String PICTURES_TABLENAME = "pictures";
 	public static final String PICTURES_COLUMN_ID = "id";
+	public static final String PICTURES_COLUMN_MISSION = "mission";
 	public static final String PICTURES_COLUMN_SENSORS = "sensors_id";
 	public static final String PICTURES_COLUMN_FILE_NAME = "name";
 	public static final String PICTURES_COLUMN_FILE_DATA = "data_base64";
