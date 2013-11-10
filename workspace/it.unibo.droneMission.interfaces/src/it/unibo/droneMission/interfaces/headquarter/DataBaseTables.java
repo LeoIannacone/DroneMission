80f4f1fbba25203b6c6928a927d6455f717d2340
Leo Iannacone <l3on@ubuntu.com>
2013-11-10 15:29:18 +0100

    update with better names database tables

--- workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/DataBaseTables.java
+++ workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/DataBaseTables.java
@@ -5,13 +5,13 @@ public class DataBaseTables {
 	// Missions table
 	public static final String MISSIONS_TABLENAME = "missions";
 	public static final String MISSIONS_ID = "id";
-	public static final String MISSIONS_START = "starts";
-	public static final String MISSIONS_END = "end";
+	public static final String MISSIONS_START = "t_start";
+	public static final String MISSIONS_END = "t_end";
 	
 	// Commands table
 	public static final String COMMANDS_TABLENAME = "commands";
 	public static final String COMMANDS_COLUMN_ID = "id";
-	public static final String COMMANDS_COLUMN_MISSION = "mission";
+	public static final String COMMANDS_COLUMN_MISSION = "mission_id";
 	public static final String COMMANDS_COLUMN_TYPE = "type";
 	public static final String COMMANDS_COLUMN_TIME = "time";
 	public static final String COMMANDS_COLUMN_VALUE = "value";
@@ -27,7 +27,7 @@ public class DataBaseTables {
 	// Notifies table
 	public static final String NOTIFIES_TABLENAME = "notifies";
 	public static final String NOTIFIES_COLUMN_ID = "id";
-	public static final String NOTIFIES_COLUMN_MISSION = "mission";
+	public static final String NOTIFIES_COLUMN_MISSION = "mission_id";
 	public static final String NOTIFIES_COLUMN_TYPE = "type";
 	public static final String NOTIFIES_COLUMN_VALUE = "value";
 	public static final String NOTIFIES_COLUMN_TIME = "time";
@@ -35,14 +35,14 @@ public class DataBaseTables {
 	// Sensors table
 	public static final String SENSORS_TABLENAME = "sensors";
 	public static final String SENSORS_COLUMN_ID = "id";
-	public static final String SENSORS_COLUMN_MISSION = "mission";
+	public static final String SENSORS_COLUMN_MISSION = "mission_id";
 	public static final String SENSORS_COLUMN_TIME = "time";
 	public static final String SENSORS_COLUMN_DATA = "data";
 	
 	// Pictures table
 	public static final String PICTURES_TABLENAME = "pictures";
 	public static final String PICTURES_COLUMN_ID = "id";
-	public static final String PICTURES_COLUMN_MISSION = "mission";
+	public static final String PICTURES_COLUMN_MISSION = "mission_id";
 	public static final String PICTURES_COLUMN_SENSORS = "sensors_id";
 	public static final String PICTURES_COLUMN_FILE_NAME = "name";
 	public static final String PICTURES_COLUMN_FILE_DATA = "data_base64";
