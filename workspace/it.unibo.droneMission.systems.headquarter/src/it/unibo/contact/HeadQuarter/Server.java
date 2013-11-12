57e5b3fffa8d07a570bc4c71731e1ff1dc49d94a
Leo Iannacone <l3on@ubuntu.com>
2013-11-12 15:43:22 +0100

    new functions to forward command - we do not need to implement others

--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/HeadQuarter/Server.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/HeadQuarter/Server.java
@@ -1,34 +1,71 @@
 package it.unibo.contact.HeadQuarter;
 
+import it.unibo.droneMission.interfaces.headquarter.IStorage;
+import it.unibo.droneMission.interfaces.messages.ICommand;
+import it.unibo.droneMission.interfaces.messages.IReply;
+import it.unibo.droneMission.messages.Factory;
+import it.unibo.droneMission.messages.Utils;
+import it.unibo.droneMission.systems.headquarter.FactoryStorage;
+
 public class Server extends ServerSupport {
 
+	protected IStorage storage;
+	
 	public Server(String name) throws Exception {
 		super(name);
-		// TODO Auto-generated constructor stub
+		storage = FactoryStorage.getInstance(FactoryStorage.MYSQL);
+	}
+	
+	public IReply forwardCommand(ICommand command) {
+		String cmd = Utils.adaptJSONToContact(command.toJSON());
+		try {
+			curAcquireOneReply = hl_server_demand_forwardCommand_controlUnit(cmd);
+			curReply = curAcquireOneReply.acquireReply();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		curReplyContent = curReply.msgContent();
+		reply =curInputMsgContent;
+		IReply r = Factory.createReply(Utils.cleanJSONFromContact(reply));
+		return r;
+	}
+	
+	public IReply forwardCommand(String commandJSON) {
+		ICommand c = Factory.createCommand(commandJSON);
+		return forwardCommand(c);
 	}
 
 	@Override
-	protected int showReplyToCommand(String reply) throws Exception {
+	protected String showReplyToCommand(String reply) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		return null;
 	}
 
 	@Override
-	protected String getSensorsData(String time) throws Exception {
+	protected String getSensorsData(String mission_id) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String getMission(String id) throws Exception {
+	protected String getPicturePackage(String mission_id) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected String getPicturePackage(String time) throws Exception {
+	protected String getNotifies(String mission_id) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	protected String getMission(String mission_id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
