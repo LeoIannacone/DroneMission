8d5661f5f0c449ba4fd32156d19056ab328a4ff9
Leo Iannacone <l3on@ubuntu.com>
2013-11-12 15:42:41 +0100

    adapt and clean json from contact

--- workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/HeadQuarter/ControlUnit.java
+++ workspace/it.unibo.droneMission.systems.headquarter/src/it/unibo/contact/HeadQuarter/ControlUnit.java
@@ -9,6 +9,7 @@ import it.unibo.droneMission.interfaces.messages.TypesCommand;
 import it.unibo.droneMission.interfaces.messages.TypesReply;
 import it.unibo.droneMission.messages.Factory;
 import it.unibo.droneMission.messages.Reply;
+import it.unibo.droneMission.messages.Utils;
 import it.unibo.droneMission.systems.headquarter.FactoryStorage;
 
 public class ControlUnit extends ControlUnitSupport {
@@ -23,6 +24,7 @@ public class ControlUnit extends ControlUnitSupport {
 	@Override
 	protected void storeDataSensors(String sensorsDatasReceived)
 			throws Exception {
+		sensorsDatasReceived = Utils.cleanJSONFromContact(sensorsDatasReceived);
 		ISensorsData s = Factory.createSensorsData(sensorsDatasReceived);
 		storage.storeSensorsData(s);		
 	}
@@ -30,12 +32,14 @@ public class ControlUnit extends ControlUnitSupport {
 	@Override
 	protected void storePicturePackage(String picturePackageReceived)
 			throws Exception {
+		picturePackageReceived = Utils.cleanJSONFromContact(picturePackageReceived);
 		IPicturePackage p = Factory.createPicturePackage(picturePackageReceived);
 		storage.storePicturePackage(p);
 	}
 
 	@Override
 	protected boolean checkCommandStart(String command) throws Exception {
+		command = Utils.cleanJSONFromContact(command);
 		ICommand c = Factory.createCommand(command);
 		return c.getType() == TypesCommand.START_MISSION || c.getType() == TypesCommand.SPEED_SET;
 	}
@@ -44,11 +48,13 @@ public class ControlUnit extends ControlUnitSupport {
 	protected String getWrongStartCommandReply() throws Exception {
 		Reply r = new Reply(TypesReply.REPLY_FAIL);
 		r.setValue("Wrong start mission command");
-		return r.toJSON();
+		return Utils.adaptJSONToContact(r.toJSON());
 	}
 
 	@Override
 	protected void storeCommandAndReply(String c, String r) throws Exception {
+		c = Utils.cleanJSONFromContact(c);
+		r = Utils.cleanJSONFromContact(r);
 		ICommand command = Factory.createCommand(c);
 		IReply reply = Factory.createReply(r);
 		storage.storeCommandAndReply(command, reply);
