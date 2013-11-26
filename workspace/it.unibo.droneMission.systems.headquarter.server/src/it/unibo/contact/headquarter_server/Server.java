4a273162b685c2df4882473dc2cb69426d8af258
Leo Iannacone <leo.iannacone@gmail.com>
2013-11-26 11:31:06 +0100

    new project for Server subject

---
+++ workspace/it.unibo.droneMission.systems.headquarter.server/src/it/unibo/contact/headquarter_server/Server.java
@@ -0,0 +1,19 @@
+package it.unibo.contact.headquarter_server;
+
+import it.unibo.contact.headquarter_server.ServerSupport;
+
+
+public class Server extends ServerSupport {
+
+	public Server(String name) throws Exception {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doJob() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
