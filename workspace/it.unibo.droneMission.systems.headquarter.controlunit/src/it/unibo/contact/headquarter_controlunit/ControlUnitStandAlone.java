bc0a59f945241e0c9b2acc7ee360bdf185b96952
Leo Iannacone <leo.iannacone@gmail.com>
2013-11-28 12:35:24 +0100

    set title in enviroment and socket timeout to 600 sec

--- workspace/it.unibo.droneMission.systems.headquarter.controlunit/src/it/unibo/contact/headquarter_controlunit/ControlUnitStandAlone.java
+++ workspace/it.unibo.droneMission.systems.headquarter.controlunit/src/it/unibo/contact/headquarter_controlunit/ControlUnitStandAlone.java
@@ -1,10 +1,19 @@
 package it.unibo.contact.headquarter_controlunit;
 
+import it.unibo.baseEnv.basicFrame.EnvFrame;
+
+
 public class ControlUnitStandAlone extends SubSystemHeadQuarterMain {
 	
 	public static void main(String args[]) throws Exception {
-		SubSystemHeadQuarterMain system = new SubSystemHeadQuarterMain( );
+		SubSystemHeadQuarterMain system = new ControlUnitStandAlone( );
 		system.doJob();
+		System.setProperty("inputTimeOut", "600000");
 	}
-	
+	protected void initGui(){
+		 env = new EnvFrame( "ControlUnitStandAlone", this );
+		 env.init();
+		 env.writeOnStatusBar( Thread.currentThread() +  " | ControlUnitStandAlone working ...",14);
+		 view = env.getOutputView();
+	}		
 }
