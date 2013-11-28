b507483b7f2b4d4a6372c564ff1265abb8733c4d
Leo Iannacone <leo.iannacone@gmail.com>
2013-11-28 18:50:44 +0100

    trying to get a greater socket timeout

--- workspace/it.unibo.droneMission.systems.drone/src/it/unibo/contact/UAV/DroneStandAlone.java
+++ workspace/it.unibo.droneMission.systems.drone/src/it/unibo/contact/UAV/DroneStandAlone.java
@@ -4,7 +4,7 @@ public class DroneStandAlone extends SubSystemDroneMain {
 	
 	public static void main(String args[]) throws Exception {
 		SubSystemDroneMain system = new SubSystemDroneMain( );
-		System.setProperty("inputTimeOut", "600000");
 		system.doJob();
-		}
+		System.setProperty("inputTimeOut", "600000");
+	}
 }
