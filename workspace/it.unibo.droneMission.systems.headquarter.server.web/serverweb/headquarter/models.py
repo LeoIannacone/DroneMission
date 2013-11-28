1366de9f24c0f5c6349ab18a6c742133a94ff912
Leo Iannacone <leo.iannacone@gmail.com>
2013-11-28 16:27:46 +0100

    update models to use server and storage

--- workspace/it.unibo.droneMission.systems.headquarter.server.web/serverweb/headquarter/models.py
+++ workspace/it.unibo.droneMission.systems.headquarter.server.web/serverweb/headquarter/models.py
@@ -1,9 +1,10 @@
 #from django.db import models
-from it.unibo.contact.HeadQuarter import Server
+from it.unibo.droneMission.systems.headquarter.storage import FactoryStorage
+from it.unibo.contact.headquarter_server import ServerStandAlone
 
-server = Server("django serverweb")
-storage = server.getStorage()
-#storage.setDebug(3)
+server = ServerStandAlone()
+storage = FactoryStorage.getInstance(FactoryStorage.MYSQL)
+storage.setDebug(3)
 
 # class Commands(models.Model):
 #     id = models.IntegerField(primary_key=True)
