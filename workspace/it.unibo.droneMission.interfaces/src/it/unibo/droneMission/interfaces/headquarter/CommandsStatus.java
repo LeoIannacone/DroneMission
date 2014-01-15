95544db662077e203c95da0984fe24d7970c7dd2
Carlo Antenucci <carlo.antenucci@gmail.com>
2014-01-15 15:32:21 +0100

    regenerated ecore diagrams in workspace

--- workspace/it.unibo.droneMission.interfaces/src/it/unibo/droneMission/interfaces/headquarter/CommandsStatus.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.droneMission.interfaces.headquarter;
-
-public class CommandsStatus {
-	
-	public static final int TO_SEND = 0;
-	public static final int SENT = 1;
-	public static final int REPLIED = 2;
-
-}
